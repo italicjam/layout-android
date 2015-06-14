28b946af4cedead5a4f299d10fc9b1359ca539af
italicjam89 <fidel.lc@gmail.com>
2015-06-14 10:03:59 -0500

    Subiendo archivo

--- Layouts/gen/edu/continental/layout/R.java
+++ Layouts/gen/edu/continental/layout/R.java
@@ -24,18 +24,51 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f08000d;
+        public static final int backbutton=0x7f080000;
+        public static final int btnAbsolute=0x7f080003;
+        public static final int btnFrame=0x7f080004;
+        public static final int btnLinear=0x7f080001;
+        public static final int btnRelative=0x7f080002;
+        public static final int btnTable=0x7f080005;
+        public static final int dates=0x7f08000b;
+        public static final int editFirstName=0x7f080007;
+        public static final int editLastName=0x7f080008;
+        public static final int firstName=0x7f080006;
+        public static final int lastName=0x7f080009;
+        public static final int name=0x7f08000a;
+        public static final int times=0x7f08000c;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_absolute_layout=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int activity_relative_layout=0x7f030002;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int absolute_layout=0x7f070000;
+        public static final int main=0x7f070001;
+        public static final int relative_layout=0x7f070002;
     }
     public static final class string {
+        public static final int absolute_layout=0x7f050005;
         public static final int action_settings=0x7f050001;
+        public static final int apellidos=0x7f05000b;
         public static final int app_name=0x7f050000;
+        public static final int done=0x7f05000d;
+        public static final int frame_layout=0x7f050006;
         public static final int hello_world=0x7f050002;
+        /**  Linear Layout 
+         */
+        public static final int linear_layout=0x7f050003;
+        public static final int nombres=0x7f05000a;
+        /**  Relative Layout 
+         */
+        public static final int regresar=0x7f050009;
+        public static final int relative_layout=0x7f050004;
+        public static final int reminder=0x7f05000c;
+        public static final int table_layout=0x7f050007;
+        public static final int title_activity_absolute_layout=0x7f05000e;
+        public static final int title_activity_relative_layout=0x7f050008;
     }
     public static final class style {
         /** 
--- Layouts/src/edu/continental/layout/MainActivity.java
+++ Layouts/src/edu/continental/layout/MainActivity.java
@@ -1,15 +1,39 @@
 package edu.continental.layout;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements android.view.View.OnClickListener {
 
+	//Declaramos los 5 botones
+	Button btnLinear;
+	Button btnFrame;
+	Button btnAbsolute;
+	Button btnRelative;
+	Button btnTable;
+	
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        //Instanciamos los botones
+        btnLinear = (Button) findViewById(R.id.btnLinear);
+        btnFrame = (Button) findViewById(R.id.btnFrame);
+        btnAbsolute = (Button) findViewById(R.id.btnAbsolute);
+        btnRelative = (Button) findViewById(R.id.btnRelative);
+        btnTable = (Button) findViewById(R.id.btnTable);
+        
+        btnLinear.setOnClickListener(this);
+        btnFrame.setOnClickListener(this);
+        btnAbsolute.setOnClickListener(this);
+        btnRelative.setOnClickListener(this);
+        btnTable.setOnClickListener(this);
     }
 
 
@@ -19,5 +43,19 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
+
+    //Metodo onclick
+	@Override
+	public void onClick(View v) {
+		
+		if ( v == btnRelative ) {
+			startActivity(new Intent(this, RelativeLayout.class));
+		} else if ( v == btnAbsolute ) {
+			//Deprecated
+			startActivity(new Intent(this, AbsoluteLayout.class));
+		}
+		
+		
+	}
     
 }
