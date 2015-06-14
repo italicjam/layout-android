e85462888e8a738a034feabf82f3620ff73e4768
italicjam89 <fidel.lc@gmail.com>
2015-06-14 12:11:36 -0500

    subiendo table layout

--- Layouts/gen/edu/continental/layout/R.java
+++ Layouts/gen/edu/continental/layout/R.java
@@ -43,11 +43,13 @@ public final class R {
         public static final int activity_absolute_layout=0x7f030000;
         public static final int activity_main=0x7f030001;
         public static final int activity_relative_layout=0x7f030002;
+        public static final int activity_table_layout=0x7f030003;
     }
     public static final class menu {
         public static final int absolute_layout=0x7f070000;
         public static final int main=0x7f070001;
         public static final int relative_layout=0x7f070002;
+        public static final int table_layout=0x7f070003;
     }
     public static final class string {
         public static final int absolute_layout=0x7f050005;
@@ -72,6 +74,7 @@ public final class R {
         public static final int table_layout=0x7f050007;
         public static final int title_activity_absolute_layout=0x7f05000e;
         public static final int title_activity_relative_layout=0x7f050008;
+        public static final int title_activity_table_layout=0x7f050010;
     }
     public static final class style {
         /** 
